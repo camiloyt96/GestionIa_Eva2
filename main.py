344947325c171f2d3125543283a30e40ea8e2ee0
EC2 Default User <ec2-user@ip-172-31-83-27.ec2.internal>
2026-05-24 22:20:14 +0000

    feat: subida a base de datos

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
 import time
 import logging as log
+
+load_dotenv()
+
 from scripts.ingesta_datos import ejecutar_ingesta
 from scripts.limpieza_datos import ejecutar_limpieza
-from scripts.validacion import ejecutar_validacion # Verifica que el archivo se llame validacion_datos.py
+from scripts.validacion import ejecutar_validacion 
 from scripts.entrenamiento_ia import entrenar_modelo_churn
 
 def run_pipeline():
@@ -50,6 +56,25 @@ def dataPipeline(csv_path):
     else:
         print(" Error: No se pudo iniciar el pipeline por fallo en la ingesta.")
         return None
+    
+    print("Iniciando subida a la base de datos RDS...")
+    try:
+        db_host = os.getenv("DB_HOST")
+        db_user = os.getenv("DB_USER")
+        db_password = os.getenv("DB_PASSWORD")
+        db_name = os.getenv("DB_NAME", "postgres")
+        db_port = os.getenv("DB_PORT", "5432")
+
+        # Crear conexión
+        connection_string = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+        engine = create_engine(connection_string)
+        
+        # Subir el dataset final a RDS
+        df_final.to_sql('dataset_churn_final', engine, if_exists='replace', index=False)
+        print("¡Dataset subido exitosamente a Amazon RDS!")
+        
+    except Exception as e:
+        print(f"Error al subir a la base de datos: {e}")
 
 if __name__ == "__main__":
     run_pipeline()
